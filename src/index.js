c620e37f1e80b831b65c07a617b1873223322a29
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-23 17:43:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 
-// const DEBOUNCE_DELAY = 300;
-// const refs = {
-//     input: document.querySelector("#search-box")
-// };
+const DEBOUNCE_DELAY = 300;
+const refs = {
+    input: document.querySelector("#search-box"),
+    list: document.querySelector('.markup'),
+    countryList: document.querySelector(".country-list"),
+};
 
-// refs.input, addEventListener('input', (evt) => {
+// function markupC() {};
 
-// });
+refs.input.addEventListener('input', (evt) => {
+    const inputName = refs.input.value
 
+    fetch(`https://restcountries.com/v3.1/name/${inputName}`)
+        .then(respone => {
+            return respone.json();
+        })
+        .then(nameCountri => {
+            console.log(nameCountri.name)
+        })
+        .then(name => {
+        })
 
-fetch ('https://restcountries.com/v3.1/name/Colombia').then(data => {
-    console.log(data)
-})
+});
+
 
