183fb8abbdae44905d51058e77b3aa5d30180df9
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-23 14:40:04 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+
+// const DEBOUNCE_DELAY = 300;
+// const refs = {
+//     input: document.querySelector("#search-box")
+// };
+
+// refs.input, addEventListener('input', (evt) => {
+
+// });
 
 
-const DEBOUNCE_DELAY = 300;
+const r = fetch ('https://restcountries.com/v3.1/name/Colombia')
+console.log(r)
+// .then(response => {
+// return response.json()
+// })
+// .then(data => {
+
+// })
+// .catch(error => {
+    
+// })
