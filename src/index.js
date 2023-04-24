0381584903c9d182439624ea6270e572b043f2c8
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-24 23:57:35 +0300

    add

--- src/index.js
+++ src/index.js
@@ -8,34 +8,50 @@ const refs = {
     countryList: document.querySelector(".country-list"),
 };
 
-function markupC() {
-    return data.map(
-        ({ flags, name, capital, population, lenguages }) =>
-            `
-            <img src="${flags.svg}" alt="flags" class="" >
-            <h1 class="name_js">Name:${name.official}</h1>
-            <p class="capital_js">Capital:${capital}</p>
-            <p class="population_js">Population:${population}</p>
-            <p class="languages_js">Languages:${lenguages}</p>
-            `
-    );
-};
-
-const data = refs.input.addEventListener('input', (evt) => {
+refs.input.addEventListener('input', (evt) => {
     const inputName = refs.input.value
 
-    if (inputName !== "") {
-        fetch(`https://restcountries.com/v3.1/name/${inputName}?fields=name,capital,population,flags,languages`)
-            .then(respone => {
-                return respone.json();
-            })
-            .then(countris => {
-                if (countris.length > 10) {
-                    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-                    return;
-                }
-                markupC(countris)
-            })
-    }
+    fetch(`https://restcountries.com/v3.1/name/${inputName}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+
+                return;
+            }
+            markupC(data)
+            // console.log(data)
+
+        })
 
 });
+
+
+
+function markupC(data) {
+    console.log(data)
+
+    if (data.length>1) {
+        const markup = data
+        .map(({capital, flags}) => `<li class="list-item new"><img src="${flags.svg}" alt="" width="40" class="flag">${capital}</li>`)
+        .join("");
+
+        refs.countryInfo.innerHTML = markup;
+    }
+
+    if (data.length=1) {
+        const markup = data
+        .map(({capital, flags}) => `<li class="list-item new"><img src="${flags.svg}" alt="" width="40" class="flag">${capital}</li>`)
+        .join("");
+
+    refs.countryInfo.innerHTML = markup;
+    }
+
+}
+
+
