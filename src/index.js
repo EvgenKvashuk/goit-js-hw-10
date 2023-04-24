9050e8019cc0359a1649cc5742453ab01d4a7361
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-04-25 02:18:08 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+// залишилось реалізувати:
+// винести функцію фетч в окремий джс файл і зробити імнований експорт
+//  дебаунс
+//  перемикання між інтерфейсом для результатів декількох країн та однієї
+// очишення інтерфейсу якщо поле пошику очищається
+// санітизація введенного рядка за допомогою trim()
+// описати стілі для розмітки
+
+
+
+
+
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import debounce from "lodash.debounce";
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -21,29 +34,27 @@ refs.input.addEventListener('input', (evt) => {
         .then(data => {
             if (data.length > 10) {
                 Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-
                 return;
             }
             markupC(data)
-            // console.log(data)
-
         })
+        .catch(error => {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+        });
 
 });
-
-
 
 function markupC(data) {
     console.log(data)
 
     if (data.length > 1) {
         const markup = data
-            .map(({ capital , flags}) =>
+            .map(({ capital, flags }) =>
                 `<li class="country-item"><img src="${flags.svg}" alt="" width="40">${capital}</li>`)
             .join("");
 
         refs.countryList.insertAdjacentHTML("afterbegin", markup);
-    }
+    };
 
     if (data.length = 1) {
         const markup = data
@@ -58,8 +69,6 @@ function markupC(data) {
             .join("");
 
         refs.countryInfo.insertAdjacentHTML("afterbegin", markup);
-    }
+    };
 
-}
-
-
+};
